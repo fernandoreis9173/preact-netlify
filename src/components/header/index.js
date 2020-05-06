41ba2f8cbdf843635dc92e776d9b85bdae925276
fernando reis <fernandopw_15cellfaire@hotmail.com>
2020-05-05 21:59:20 -0400

    test

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,8 +6,8 @@ const Header = () => (
 	<header class={style.header}>
 		<Link href="/"><h1>Fernando Reis</h1></Link>
 		<nav>
-			<Link activeClassName={style.active} to="//www.linkedin.com/in/fernando-reis-79b643144/">LinkedIN</Link>
-			<Link activeClassName={style.active} to="//github.com/fernandoreis9173">GitHub</Link>
+			<Link activeClassName={style.active} ><a href="http//www.linkedin.com/in/fernando-reis-79b643144/">LinkedIN</a></Link>
+			<Link activeClassName={style.active} ><a href="http//github.com/fernandoreis9173">GitHub</a></Link>
 			<Link activeClassName={style.active} href="/blogs">Sobre Mim</Link>
 			<Link activeClassName={style.active} href="/contact">Contatos</Link>
 		</nav>
